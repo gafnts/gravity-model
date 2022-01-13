7f7d6e73cd63b0242b676bb49557e1dd6f8380ec
Gabriel Fuentes <gafnts@gmail.com>
2022-01-13 09:42:22 -0600

    GUF removal

--- Data/Exports — Data extraction.R
+++ Data/Exports — Data extraction.R
@@ -58,7 +58,7 @@ sa_exports_fst <-
 write(sa_exports_fst)
 
 sa_exports_snd <-
-  get(partners = c("Colombia", "Ecuador", "French Guiana", "Venezuela", "Guyana"),
+  get(partners = c("Colombia", "Ecuador", "Venezuela", "Guyana"),
       years = c(1995:2020))
 write(sa_exports_snd)
 
